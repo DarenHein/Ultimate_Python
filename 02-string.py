0ca164aa269a54b758f0cf8432797794ee01eb15
DarenHein <darenhein@hotmail.com>
2023-06-17 07:38:43 -0600

    v2

---
+++ 02-string.py
@@ -0,0 +1,10 @@
+# metodos con string 
+# todo metodo es una funcion y esto es una accion 
+
+texto = "Hola Mundo "
+
+print (texto.upper()) # todo Convierte todo a myusculas 
+print (texto.lower()) # todo Convierte todo a minusculas  
+print (texto.find("M")) # todo Busca como si fuera una rrya donde se encuentra la letra indicada cen los parentesis 
+print (texto.replace("M","Putom")) # todo busca la cadena ingresada y la cambia por la cadena separada por la coma 
+
