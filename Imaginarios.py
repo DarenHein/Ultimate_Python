eac35588ae6d17647872e44a95480053d462eb36
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-06-18 03:21:54 -0600

    vw

---
+++ Imaginarios.py
@@ -0,0 +1,14 @@
+
+# todo numeros imaginarios python tambien los maneja 
+
+numero = 7j #! j es la parte imaginara 
+numero2 = 8j
+resultado = numero + numero2 #! como una suma cualquiera de numeros 
+resultado2 = numero - numero2  
+resultado3 = numero * numero2  
+resultado4 = numero / numero2  
+print(f"el resultado es {resultado}")
+print(f"el resultado es {resultado2}")
+print(f"el resultado es {resultado3}")
+print(f"el resultado es {resultado4}")
+print(type(resultado)) #! devuelve un complejo 
