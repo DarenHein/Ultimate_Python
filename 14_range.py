4bd68c2e9346a4406d27dd1e01c57a654c12c010
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-06-20 23:52:59 -0600

    v2

---
+++ 14_range.py
@@ -0,0 +1,12 @@
+'''
+range es un metodo de python 
+se oucpa la mayoria en for 
+range (star,stop,step) 
+star -> inicio 
+stop -> parar 
+step -> pasos 
+'''
+i = 0 
+
+for i in range(i,11,i+1): 
+    print(i)
---
+++ Ejercicios/For_1.py
@@ -0,0 +1,46 @@
+
+# calcular la suma de los numeros del 1 al 100 
+
+i = 0 
+aux = 0
+
+for i in range(i , 101,i+1): 
+    aux = aux + i
+
+print(aux)
+
+#Imprimir los elemntos de una lista 
+
+lista=[1,2,3,4,5,6]
+
+for i in  lista : 
+    print(i)
+
+#imprimir cada caracter de una palabra 
+
+nombre = "Luis"
+i = 4 
+
+for i in nombre : 
+    print(i)
+#imprimir los nuermos del 1al 20 
+
+i = 0 
+for i in range(i,21,i+1): 
+    print(i)
+
+#facroial de un numero 
+
+#neceitsmoa el numero al cual sacar el facotiral 
+numero = 5
+#necesitamos un numero que almacene el valor final 
+factorial = 1 
+i = 1 
+for i in range(i,numero+1):
+    factorial= factorial * i 
+    
+
+print(factorial)
+
+    
+
